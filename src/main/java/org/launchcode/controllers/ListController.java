9a00b85c1a70d463370548354216ff93d860a9f7
Marco Torrente Corbett <mtorrente@stlz106.local>
2017-12-04 19:09:24 -0600

    First commit

--- src/main/java/org/launchcode/controllers/ListController.java
+++ src/main/java/org/launchcode/controllers/ListController.java
@@ -41,6 +41,7 @@ public class ListController {
             ArrayList<HashMap<String, String>> jobs = JobData.findAll();
             model.addAttribute("title", "All Jobs");
             model.addAttribute("jobs", jobs);
+            model.addAttribute("jobResults", jobs);
             return "list-jobs";
         } else {
             ArrayList<String> items = JobData.findAll(column);
@@ -54,11 +55,12 @@ public class ListController {
 
     @RequestMapping(value = "jobs")
     public String listJobsByColumnAndValue(Model model,
-            @RequestParam String column, @RequestParam String value) {
+                                           @RequestParam String column, @RequestParam String value) {
 
         ArrayList<HashMap<String, String>> jobs = JobData.findByColumnAndValue(column, value);
         model.addAttribute("title", "Jobs with " + columnChoices.get(column) + ": " + value);
         model.addAttribute("jobs", jobs);
+
 
         return "list-jobs";
     }
